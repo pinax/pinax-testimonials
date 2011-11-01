00543d35cc8527a8e79e44b882dfa48d8b1375f3
Patrick Altman <paltman@gmail.com>
2011-10-31 22:58:45 -0500

    Fix random attribution bug

--- marturion/templatetags/marturion_tags.py
+++ marturion/templatetags/marturion_tags.py
@@ -12,25 +12,30 @@ class TestimonialsNode(template.Node):
     def handle_token(cls, parser, token, order_by, **kwargs):
         bits = token.split_contents()
         
-        if len(bits) != 3:
-            raise template.TemplateSyntaxError("%r takes exactly two arguments "
-                "(first argument must be 'as')" % bits[0])
-        if bits[1] != "as":
+        if len(bits) != 4:
+            raise template.TemplateSyntaxError("%r takes exactly three arguments "
+                "(second argument must be 'as')" % bits[0])
+        if bits[2] != "as":
             raise template.TemplateSyntaxError("Second argument to %r must be "
                 "'as'" % bits[0])
         
-        return cls(bits[2], order_by)
+        return cls(bits[2], bits[3], order_by)
     
-    def __init__(self, context_var, order_by):
+    def __init__(self, how_many, context_var, order_by):
+        self.how_many = how_many
         self.context_var = context_var
         self.order_by = order_by
     
     def render(self, context):
-        context[self.context_var] = Testimonial.objects.filter(
-            active=True
-        ).order_by(
-            self.order_by
-        )
+        how_many = self.how_many.resolve(context)
+        context[self.context_var] = [
+            t
+            for t in Testimonial.objects.filter(
+                active=True
+            ).order_by(
+                self.order_by
+            )[:how_many]
+        ]
         return u""
 
 
