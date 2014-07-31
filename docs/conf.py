74ad8d4d80d4f84e7064f1912fc43984f6a8098b
James Tauber <jtauber@jtauber.com>
2014-07-31 14:50:51 -0400

    docs and setup flake8 compliance

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,8 @@ html_theme = 'default'
 html_static_path = []
 htmlhelp_basename = 'marturiondoc'
 latex_documents = [
-  ('index', 'marturion.tex', u'marturion Documentation',
-   u'Eldarion', 'manual'),
+    ('index', 'marturion.tex', u'marturion Documentation', u'Eldarion', 'manual'),
 ]
 man_pages = [
-    ('index', 'marturion', u'marturion Documentation',
-     [u'Eldarion'], 1)
+    ('index', 'marturion', u'marturion Documentation', [u'Eldarion'], 1),
 ]
